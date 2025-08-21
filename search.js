0e682ce543ae6b427071430dc6865f94ba8dcd4a
AdamSadowsky <adamsadowsky@gmail.com>
2025-08-20 23:20:04 -0400

    HTML *Implement a search bar *Add section placeholder for dynamic JS content CSS *Style card layout and format *Center search bar JS *Instantiate TMDB API *Render fecthed data to card layout

---
+++ search.js
@@ -0,0 +1,47 @@
+const APILINK  = 'http://localhost:3000/discover';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCHAPI = 'http://localhost:3000/search?query=';
+
+const main = document.getElementById('section');
+const form = document.getElementById('form');
+const search = document.getElementById('query');
+
+displayMovies(APILINK);
+
+function displayMovies(url){
+    fetch(url).then(res => res.json())
+    .then(function(data){
+        console.log(data.results);
+        data.results.forEach(el => {
+            const divCard = document.createElement('div');
+            divCard.classList.add('card');
+            const divRow = document.createElement('div');
+            divRow.classList.add('row');
+            const divCol = document.createElement('div');
+            divCol.classList.add('column');
+            const poster = document.createElement('img');
+            poster.classList.add('poster');
+            poster.src = IMG_PATH + el.poster_path;
+            poster.setAttribute('data-movie-id', el.id);
+            const title = document.createElement('h3');
+            title.classList.add('movie_title');
+            title.textContent = el.title;
+
+            divCard.appendChild(poster);
+            divCard.appendChild(title);
+            divCol.appendChild(divCard);
+            divRow.appendChild(divCol);
+            main.appendChild(divRow);
+        });
+    });
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    main.innerHTML = '';
+    const searchItem = search.value;
+    if(searchItem){
+        displayMovies(SEARCHAPI + searchItem);
+        search.value = searchItem;
+    }
+});
